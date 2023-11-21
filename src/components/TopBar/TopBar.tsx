dfa56e1599bbcc75c874b004a4fa8068f48b3786
gouyeonch <gouyeonch@naver.com>
2023-11-21 20:33:47 +0900

    feat: init components/TopBar

---
+++ src/components/TopBar/TopBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { S } from "./style";
+
+const TopBar : React.FC = () => {
+    return (
+      <>
+        
+      </>
+    );
+  };
+  
+  export default TopBar;
