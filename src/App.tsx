a9bf14539c068707a789d615ee049b0315db8460
gouyeonch <gouyeonch@naver.com>
2023-11-22 16:28:40 +0900

    feat: components/BodyTitle

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import viteLogo from '/vite.svg'
 import SidebarAdmin from './components/Sidebar/SidebarAdmin'
 import SidebarUser from './components/Sidebar/SidebarUser'
 import TopBar from './components/TopBar/TopBar'
+import BodyTitle from './components/BodyTitle/BodyTitle'
 import './App.css'
 
 function App() {
   return (
     <>
-      <TopBar pageName='페이지 이름'></TopBar>
+      <TopBar pageName='페이지 이름'/>
       {/* <SidebarUser /> */}
       <SidebarAdmin />
+      <BodyTitle bodyName='마이페이지'/>
     </>
   )
 }
