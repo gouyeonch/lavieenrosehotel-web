9eb77b5b3e6783b1b0f809a31bfb0d82fcf5072b
gouyeonch <gouyeonch@naver.com>
2023-11-25 15:29:35 +0900

    feat: CheckResvTraf/ResvNotFound

--- src/pages/CheckResvTraf/CheckResvTraf.tsx
+++ src/pages/CheckResvTraf/CheckResvTraf.tsx
@@ -4,6 +4,7 @@ import TopBar from "../../components/TopBar/TopBar";
 import SidebarUser from "../../components/Sidebar/SidebarUser";
 import BodyTitle from "../../components/BodyTitle/BodyTitle";
 import ResvBox from "./ResvBox";
+import ResvNotFound from "./ResvNotFound";
 
 type ResvData = {
     paymentDate : string;
@@ -15,18 +16,18 @@ type ResvData = {
 const CheckResvRoom: React.FC = () => {
     const [resvData, setresvData] = useState<ResvData[]>([
         // dummy 데이터
-        {
-            paymentDate: "2023.09.05",
-            resvDate: "2023.09.09 ~ 09.21",
-            traf: "편도 - 홍대 <-> 리조트 -  대인5",
-            payment: "250000원"
-        },
-        {
-            paymentDate: "1999.09.05",
-            resvDate: "1999.09.09 ~ 09.21",
-            traf: "왕복 - 명동 <-> 리조트 -  대인2",
-            payment: "100000원"
-        },
+        // {
+        //     paymentDate: "2023.09.05",
+        //     resvDate: "2023.09.09 ~ 09.21",
+        //     traf: "편도 - 홍대 <-> 리조트 -  대인5",
+        //     payment: "250000원"
+        // },
+        // {
+        //     paymentDate: "1999.09.05",
+        //     resvDate: "1999.09.09 ~ 09.21",
+        //     traf: "왕복 - 명동 <-> 리조트 -  대인2",
+        //     payment: "100000원"
+        // },
     ]);
   
     return (
@@ -40,11 +41,16 @@ const CheckResvRoom: React.FC = () => {
                     <S.RightBody>
                         <BodyTitle bodyName="교통편 예약확인"/>
                         
-                        <S.ResvList>
-                            {resvData.map((resv, index) => (
-                                <ResvBox key={index} ResvData={resv} />
-                            ))}
-                        </S.ResvList>
+                        {resvData.length === 0 ? (
+                                <ResvNotFound /> // 예약이 없으면 없다는 메세지와 예약 바로가기 랜더링
+                            ) : (
+                                // 예약이 있으면 예약 리스트 랜더링
+                                <S.ResvList> 
+                                    {resvData.map((resv, index) => (
+                                        <ResvBox key={index} ResvData={resv} />
+                                    ))}
+                                </S.ResvList>
+                            )}
                     </S.RightBody>
                 </S.MainBody>
             </S.Container>
--- src/pages/CheckResvTraf/ResvNotFound.tsx
+++ src/pages/CheckResvTraf/ResvNotFound.tsx
@@ -6,7 +6,7 @@ const ResvNotFound: React.FC = () => {
   return (
     <>
         <S.NotFoundContainer>
-          <S.NotFoundText>현재 부대/복리시설 예약이 없습니다.</S.NotFoundText>
+          <S.NotFoundText>현재 교통편 예약이 없습니다.</S.NotFoundText>
           <Button buttonName="예약하러 홈페이지로 이동"/>
         </S.NotFoundContainer>
     </>
