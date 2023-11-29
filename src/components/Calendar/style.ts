7eb9a361a8e1456b90ea52c88e84537f67422bdd
jun6292 <c68254@gmail.com>
2023-11-30 00:14:23 +0900

    feat: 유저 부대/복리 시설 예약 페이지, 유저 숙박 예약 페이지 - 캘린더 컴포넌트 추가, 3개월 미만만 조회

---
+++ src/components/Calendar/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import DatePicker from 'react-datepicker';
+
+const CalendarContainer = styled.div`
+    margin-left: 50px;
+`;
+
+export const S = {
+    CalendarContainer,
+}
