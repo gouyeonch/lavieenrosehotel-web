36c3313c11ff1f2eedd4158c5cb73fdd35d81e67
gouyeonch <gouyeonch@naver.com>
2023-12-02 18:03:35 +0900

    feat: AdminAmenCatAdd 이미지 처리 필요

--- src/pages/AdminAmenCatAdd/AdminAmenCatAdd.tsx
+++ src/pages/AdminAmenCatAdd/AdminAmenCatAdd.tsx
@@ -6,20 +6,13 @@ import InputBox from "../../components/InputBox/InputBox";
 import InputBoxCnt from "../../components/InputBox/InputBoxCnt";
 import InputBoxUnit from "../../components/InputBox/InputBoxUnit";
 import InputBox4Info from "../../components/InputBox/InputBox4Info";
-import ViewBox from "../../components/ViewBox/ViewBox";
-import Report from "../../components/Report/Report";
 import Button from "../../components/Button/Button";
 
 const AdminAmenCatAdd: React.FC = () => {
     const [name, setName] = useState<string>("");
-    const [type, setType] = useState<string>("");
-    const [view, setView] = useState<string>("");
-    const [floor, setFloor] = useState<string>("");
-    const [discription, setDiscription] = useState<string>("");
-
-    const handleAccompaniedChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setAccompanied(e.target.checked);
-    };
+    const [discShort, setDiscShort] = useState<string>("");
+    const [discLarg, setDiscLarg] = useState<string>("");
+    const [cost, setCost] = useState<string>("");
 
     return (
         <>
@@ -34,15 +27,27 @@ const AdminAmenCatAdd: React.FC = () => {
                         <S.SubTitleDisc>액티비티 정보를 입력해주세요</S.SubTitleDisc>
                         <S.ColumnBox>
                             <S.RowBox>
-                                <InputBoxCnt label={"액티비티 카테고리명"} value={name} onChange={setName} count={20} width={"250px"}/>
-                                <InputBox label={"액티비티 요약설명"} value={type} onChange={setType} width={"700px"}/>    
+                                <InputBoxCnt label={"액티비티 카테고리명"} value={name} onChange={setName} count={20} width={"300px"}/>
+                                <InputBox label={"액티비티 요약설명"} value={discShort} onChange={setDiscShort} width={"700px"}/>    
                             </S.RowBox>
                             
                             <S.RowBox>
-                                <InputBox4Info label={"액티비티 추가 정보"} value={view} onChange={setView} width={"1050px"}/>
+                                <InputBox4Info label={"액티비티 추가 정보"} value={discLarg} onChange={setDiscLarg} width={"1050px"}/>
                             </S.RowBox>
                         </S.ColumnBox>
 
+                        <S.SubTitle>액티비티 이미지 업로드</S.SubTitle>
+                        <S.ColumnBox>
+                            <S.RowBox>
+                            </S.RowBox>
+                        </S.ColumnBox>
+
+                        <S.SubTitle>서비스 요금 설정</S.SubTitle>
+                        <S.ColumnBox>
+                            <S.RowBox>
+                                <InputBoxUnit label={"가격"} value={cost} onChange={setCost} unit="원" width={"250px"}/>
+                            </S.RowBox>
+                        </S.ColumnBox>
 
                         <S.ButtonBox>
                             <Button buttonName="완료 및 저장하기" buttonColor="#BFDEFA"/>
