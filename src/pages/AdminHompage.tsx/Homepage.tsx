2c61d889e05cd687db7b9a55cd133260339152f5
gouyeonch <gouyeonch@naver.com>
2023-12-02 20:06:16 +0900

    fit: App background-color

---
+++ src/pages/AdminHompage.tsx/Homepage.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { S } from "./style";
+import TopBar from "../../components/TopBar/TopBar";
+import UserTopBar from "../../components/UserTopBar/UserTopBar";
+
+const Homepage: React.FC = () => {
+  return (
+    <>
+        <S.Container>
+            <TopBar isAdmin={false} pageName="홈페이지"/>
+            <UserTopBar />
+            <S.ImgaeArea>홈페이지 그림영역</S.ImgaeArea>
+        </S.Container>
+    </>
+  );
+};
+
+export default Homepage;
