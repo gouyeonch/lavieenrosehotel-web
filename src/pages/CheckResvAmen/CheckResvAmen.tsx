201447b13fd56042134080c0eea9a740a412b979
gouyeonch <gouyeonch@naver.com>
2023-12-04 07:22:19 +0900

    feat: 예약 목록 조회

--- src/pages/CheckResvAmen/CheckResvAmen.tsx
+++ src/pages/CheckResvAmen/CheckResvAmen.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { S } from "./style";
+import apiClient from "../../api/Axios";
 import TopBar from "../../components/TopBar/TopBar";
 import SidebarUser from "../../components/Sidebar/SidebarUser";
 import BodyTitle from "../../components/BodyTitle/BodyTitle";
@@ -7,28 +8,29 @@ import ResvBox from "./ResvBox";
 import ResvNotFound from "./ResvNotFound";
 
 type ResvData = {
-    paymentDate : string;
-    resvDate : string;
-    amen : string;
-    payment : string;
+    payment_date : string;
+    start_date : string;
+    amenity_name : string;
+    total_price : string;
 };
 
 const CheckResvRoom: React.FC = () => {
-    const [resvData, setresvData] = useState<ResvData[]>([
-        // dummy 데이터
-        // {
-        //     paymentDate: "2023.09.05",
-        //     resvDate: "2023.09.09 ~ 09.21",
-        //     amen: "워터파크 - 종일권 -  대인2",
-        //     payment: "250000원"
-        // },
-        // {
-        //     paymentDate: "1999.09.05",
-        //     resvDate: "1999.09.09 ~ 09.21",
-        //     amen: "리프트패키지2 - 종일권 -  대인2",
-        //     payment: "100000원"
-        // },
-    ]);
+    const [resvData, setResvData] = useState<ResvData[]>();
+
+    useEffect(() => {
+        // 데이터를 불러옵니다.
+        const fetchData = async () => {
+            try {
+              const response = await apiClient.get(`/admin/reservation-amenities?page=1&size=10`);
+
+              setResvData(response.data.data.reservations);
+              console.log(resvData);
+            } catch (error) {
+            }
+          };
+
+        fetchData();
+      }, []);
   
     return (
         <>
@@ -41,12 +43,12 @@ const CheckResvRoom: React.FC = () => {
                     <S.RightBody>
                         <BodyTitle bodyName="부대/복리 시설예약 현황확인"/>
                         
-                        {resvData.length === 0 ? (
+                        {resvData?.length === 0 ? (
                                 <ResvNotFound /> // 예약이 없으면 없다는 메세지와 예약 바로가기 랜더링
                             ) : (
                                 // 예약이 있으면 예약 리스트 랜더링
                                 <S.ResvList> 
-                                    {resvData.map((resv, index) => (
+                                    {resvData?.map((resv, index) => (
                                         <ResvBox key={index} ResvData={resv} />
                                     ))}
                                 </S.ResvList>
